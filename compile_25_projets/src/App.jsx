3f5312eb7651ae92285e2868e8ceb2d05933b2bd
Shadowghost94 <salifououzerou25@gmail.com>
2025-01-01 01:25:52 +0100

    premier commit 01-01-2025

---
+++ compile_25_projets/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Accordian from "./components/accordeon";
+
+function App() {
+    return (
+        <>
+            {/* Notre composants d'accordéon */}
+            <Accordian />
+        </>
+    );
+}
+
+export default App;
---
+++ compile_25_projets/src/components/accordeon/data.js
@@ -0,0 +1,25 @@
+const data = [
+    {
+        id: "1",
+        question: "C'est quoi un composants d'accordéon ?",
+        answer: "Un composant React d'accordéon est un élément interactif qui permet d'afficher et de masquer dynamiquement des sections de contenu, généralement sous forme de titres cliquables, pour économiser de l'espace et organiser l'information de manière compacte",
+    },
+    {
+        id: "2",
+        question: "Elle sont utilisé dans quel but ?",
+        answer: "Elle peuvent être utilisé dans divers contexte, comme les FAQ(Foire aux questions), les menus déroulants, les options de filtrage ou les paramètres",
+    },
+    {
+        id: "3",
+        question: "Accordéon en tant qu'instrument de musique",
+        answer: "L'accordéon est un instrument de musique à vent et à clavier, où le son est produit en compressant et en décompressant un soufflet tout en pressant les touches ou les boutons, souvent utilisé dans la musique folk, traditionnelle et populaire",
+    },
+    {
+        id: "4",
+        question:
+            "Est-ce que je peux créer un composant d'accordéon avec un autre framework ?",
+        answer: "Oui il est tout à fait possible de créer un composant d'accordéon avec une autre framework",
+    },
+];
+
+export default data;
